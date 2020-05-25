0a28a47afbee23fd5666c691b9df7cfa568019f3
Елизавета <liz@MacBook-Pro-Elizaveta.local>
2020-05-25 20:47:03 +0300

    initial commit

---
+++ myFileNavLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myFileNavLight
+//
+//  Created by Елизавета on 25.05.2020.
+//  Copyright © 2020 Elizaveta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
